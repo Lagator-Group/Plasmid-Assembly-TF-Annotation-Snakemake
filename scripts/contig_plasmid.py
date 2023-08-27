a921007f3f6cf219fec0d7776d3f07d59199b27e
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2023-08-27 20:31:29 +0100

    Update contig_plasmid.py

--- scripts/contig_plasmid.py
+++ scripts/contig_plasmid.py
@@ -26,10 +26,6 @@ def get_contig(tab):
             end=data.find('>', start+1) #determines end of contig with '>'
             result=data[start:end] #isolates contig from '>' from '>'
 
-            contig_directory='contigs_plasmid'
-            if not os.path.isdir(contig_directory):
-                os.mkdir(contig_directory)
-
             _fname=str(df['#FILE'][n])[:-15]
             fname=_fname+'_'+str(df['SEQUENCE'][n])+'.fasta' #final file name example = 0flye_contig_1.fasta
             path=contig_directory+'/'+fname #contigs/0flye_contig_1.fasta
