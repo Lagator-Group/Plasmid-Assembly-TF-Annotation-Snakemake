5111a9729e36935b6e042615693a9dee76e138e1
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2024-02-08 10:22:20 +0800

    shovill assembly

---
+++ short_assembly.sh
@@ -0,0 +1,7 @@
+
+conda activate shovill
+
+for fastq in fastq/*_1.fastq ; do
+    sample=$(basename $fastq _1.fastq)
+    shovill --R1 fastq/$sample\_1.fastq --R2 fastq/$sample\_2.fastq --outdir shovill/$sample --force
+done
