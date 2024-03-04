54416c694d04d1467d3dde2f819a8ba6363080e9
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2024-03-04 18:01:25 +0000

    Delete hybrid_assembly.sh

--- hybrid_assembly.sh
+++
@@ -1,8 +0,0 @@
-conda activate unicycler
-
-for fastq in fastq/*_1.fastq ; do
-    sample=$(basename $fastq _1.fastq)
-    unicycler -1 fastq/$sample\_1.fastq -2 fastq/$sample\_2.fastq -l fastq/$sample\.fastq -o unicycler/$sample
-    mkdir fasta_wgs
-    cp unicycler/$sample/assembly.fasta fasta_wgs/$sample\.fasta
-done
