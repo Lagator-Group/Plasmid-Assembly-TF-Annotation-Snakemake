338ead46fdc8d8075ad4407c686ff9084a55196b
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2024-03-05 18:28:00 +0000

    Update deeptfactor_loci.py

--- bin/scripts/deeptfactor_loci.py
+++ bin/scripts/deeptfactor_loci.py
@@ -1,33 +1,27 @@
-# %%
 import pandas as pd
 import os
 
-# %%
+prokka = snakemake.input[0]
+
+plasmid_file = snakemake.input[1]
+plasmid_name = plasmid_file.replace('.fasta','').replace('fasta_plasmid/','')
+
 plasmid_df = pd.read_csv('plasmid_summary.csv',sep=',')
 
-# %%
-n=0
-try:
-    os.mkdir('deepTFactor_hits')
-except:
-    pass
+deepTFactor_hits = snakemake.output[0]
 
-for locid_df in plasmid_df['DeepTFactor Locus_ID']:
-    locid_list = locid_df.split(',')
-    plasmid = plasmid_df.iloc[n]['Plasmid']
-    print(plasmid)
-    deepTFactor_hits = 'deepTFactor_hits/' + plasmid
-    for locid in locid_list:
-        locid = locid.replace('[','').replace(']','').replace("'","").replace(' ','')
-        locid = '>'+locid
-        with open('prokka/' + plasmid + '/'+plasmid+'.ffn','r') as f:
-            data = f.read()
-            start = data.find(locid)
-            end = data.find('>', start+1)
-            result = data[start:end]
-            with open(deepTFactor_hits + '.fasta', 'a') as f:
-                f.write(result)
-            f.close()
-    n+=1
+location = plasmid_df.loc[plasmid_df['Plasmid']==plasmid_name,'DeepTFactor Locus_ID']
+locid_list = str(location.iloc[0])
+locid_list = locid_list.split(',')
 
-
+for locid in locid_list:
+    locid = locid.replace('[','').replace(']','').replace("'","").replace(' ','')
+    locid = '>'+locid
+    with open(prokka,'r') as f:
+        data = f.read()
+        start = data.find(locid)
+        end = data.find('>', start+1)
+        result = data[start:end]
+        with open(deepTFactor_hits, 'a') as f:
+            f.write(result)
+        f.close()
