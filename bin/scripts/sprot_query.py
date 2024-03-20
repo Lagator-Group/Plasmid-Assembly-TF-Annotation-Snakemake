584be055f3418ec61d534bded8404825b721a47c
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2024-03-20 11:13:54 +0000

    added connection error handling

--- bin/scripts/sprot_query.py
+++ bin/scripts/sprot_query.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 
 query_df=pd.DataFrame(columns=['Locus Tag','Entry'])
 query_df
@@ -17,10 +18,18 @@ with open(snakemake.input[0]) as f:
 query_df.reset_index(drop=True,inplace=True)
 
 df=pd.DataFrame(columns=['Entry','Protein names','Gene Names'])
-for i in query_df['Entry']:
-    print(i)
-    url = 'https://rest.uniprot.org/uniprotkb/'+i+'.tsv'
-    df = pd.concat([df,pd.read_csv(url, sep='\t',usecols=['Entry','Protein names','Gene Names'])])
+
+error = 0
+error_max = 10
+while error <= error_max:
+    try:
+        for i in query_df['Entry']:
+            print(i)
+            url = 'https://rest.uniprot.org/uniprotkb/'+i+'.tsv'
+            df = pd.concat([df,pd.read_csv(url, sep='\t',usecols=['Entry','Protein names','Gene Names'])])
+    except:
+        time.sleep(60)
+        error += 1
 
 query_df=query_df.merge(df,on='Entry',how='left')
 
